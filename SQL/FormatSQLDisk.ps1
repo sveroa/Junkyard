ee817d55b9230430a6132d46b0e1a0785fd297fe
Roar Jørstad <sveroa@hotmail.com>
2017-06-04 21:37:47 +0200

    Update FormatSQLDisk.ps1

--- SQL/FormatSQLDisk.ps1
+++ SQL/FormatSQLDisk.ps1
@@ -1,8 +1,14 @@
+# ----------------------------------------------------------------------------------
+# Powershell function that is used to format disks that shall be used as DATA, LOG
+# TEMPDB or local BACKUP. This disk types should be formatted with 64K block size.
+# This should be done due to reduction number of disk interactions SQL server need
+# to perform when it needs to read/write disk to access pages.
+# ----------------------------------------------------------------------------------
 function FormatSQLDisk([string]$driveletter, [string]$drivelabel)
 {
     Format-Volume 	`
 			-DriveLetter $driveletter `
 			-NewFileSystemLabel $drivelabel `
             -FileSystem NTFS `
-			-AllocationUnitSize 65536 –Force -Confirm:$false
+			-AllocationUnitSize 65536 â€“Force -Confirm:$false
 }
